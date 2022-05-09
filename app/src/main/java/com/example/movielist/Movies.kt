f62dd2deecc91053f8689647baa582ef5b847b87
Felix-Katerin <ftellezr@esuaca.ac.cr>
2022-05-09 08:03:15 -0600

    Initial commit

---
+++ app/src/main/java/com/example/movielist/Movies.kt
@@ -0,0 +1,12 @@
+package com.example.movielist
+
+
+//Sirve para crear un objeto con la información de la lista para instanciarlo
+data class Movies(
+    val NombrePeliculas:String,
+    val Movies:String,
+    val descripcion:String,
+    val genero:String,
+    val Fecha:String
+
+)
